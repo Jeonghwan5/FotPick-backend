fd24a96ec089ac876d5f90cb7f3cbb83f09f3048
20211513OhJeonghwan <20211513@daejin.ac.kr>
2026-01-19 23:30:18 +0900

    Initial FotPick-backend setup with JWT

---
+++ src/main/java/com/fotpick/fotpickbackend/FotpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fotpick.fotpickbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotpickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotpickBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fotpick/fotpickbackend/entity/User.java
@@ -0,0 +1,4 @@
+package com.fotpick.fotpickbackend.entity;
+
+public class User {
+}
